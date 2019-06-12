69335e6e795301126a678516f23ba8b5f5231781
Gareth Davies <garethdavies.london@gmail.com>
2019-06-12 08:59:42 +0100

    With latch working and one signal alpha calc
    Put a latch on for dwell time on different sig channels.
    
    Reduced the FP calc volume by having only one Alpha value.

--- SimpleAudioLights.ino
+++ SimpleAudioLights.ino
@@ -3,19 +3,22 @@
 //#define DEBUGSIGNALS
 //#define DEBUGSCALED
 
-#define SAMPLES 256
+#define SAMPLES 128
 #define OUTPUTPIN 8
 #define BASEPIN 11
 #define TREBPIN 10
 #define REVERSEPIN 9
+
 #define ALPHA 8 //This is the filter used in the Low Pass Filter
 #define SIGALPHAAGC 64   //Used in the AGC Calculation
 #define BASEALPHAAGC 64   //Used in the AGC Calculation
 #define TREBALPHAAGC 64   //Used in the AGC Calculation
 #define SIGRATCHETGAP 10 //This is the peak detect reset, when it peaks it moves the average up towards the peak, from which it then decays...
 #define BASERATCHETGAP 10 //This is the peak detect reset, when it peaks it moves the average up towards the peak, from which it then decays...
-#define TREBRATCHETGAP 40 //This is the peak detect reset, when it peaks it moves the average up towards the peak, from which it then decays...
-
+#define TREBRATCHETGAP 20 //This is the peak detect reset, when it peaks it moves the average up towards the peak, from which it then decays...
+#define SIGLATCHLENGTH 1 //Once triggered this is the on dwell time
+#define HPLATCHLENGTH 2 //Once triggered this is the on dwell time
+#define LPLATCHLENGTH 5 //Once triggered this is the on dwell time
 
   int smax=0;
   int lpmax=0;
@@ -25,14 +28,19 @@
   float lpfilter,hpfilter;
   float lpfilterLast,hpfilterLast;
   float alpha;
-  float baseAlphaAGC,trebAlphaAGC,sigAlphaAGC;
+  //float baseAlphaAGC;
+  //float trebAlphaAGC
+  float sigAlphaAGC;
   float sampleF;
 
   float average=0,lpaverage=0,hpaverage=0;
   float averageLast=0,lpaverageLast=0,hpaverageLast=0;  
 
   bool sigTrig=false,lpTrig=false,hpTrig=false;
-
+  
+  unsigned int lplatch;
+  unsigned int hplatch;
+  unsigned int siglatch;
 
   
 void setup() {
@@ -47,8 +55,8 @@ void setup() {
   pinMode(REVERSEPIN,OUTPUT);
   alpha=1.0/ALPHA;
   sigAlphaAGC=1.0/SIGALPHAAGC;
-  baseAlphaAGC=1.0/BASEALPHAAGC;
-  trebAlphaAGC=1.0/TREBALPHAAGC;
+  //baseAlphaAGC=1.0/BASEALPHAAGC;
+  //trebAlphaAGC=1.0/TREBALPHAAGC;
 }
 
 
@@ -59,6 +67,8 @@ void loop() {
   smax=0;
   lpmax=0;
   hpmax=0;
+
+
 
   
   for (int count=0;count<SAMPLES;count++)
@@ -79,27 +89,49 @@ void loop() {
   }
 
   average = (sigAlphaAGC*(float)smax) + ( (1.0-sigAlphaAGC)*averageLast );
-  lpaverage = (baseAlphaAGC*(float)lpmax) + ( (1.0-baseAlphaAGC)*lpaverageLast );
-  hpaverage = (trebAlphaAGC*(float)hpmax) + ( (1.0-trebAlphaAGC)*hpaverageLast );
+  //lpaverage = (baseAlphaAGC*(float)lpmax) + ( (1.0-baseAlphaAGC)*lpaverageLast );
+  //hpaverage = (trebAlphaAGC*(float)hpmax) + ( (1.0-trebAlphaAGC)*hpaverageLast );
+  lpaverage = (sigAlphaAGC*(float)lpmax) + ( (1.0-sigAlphaAGC)*lpaverageLast );
+  hpaverage = (sigAlphaAGC*(float)hpmax) + ( (1.0-sigAlphaAGC)*hpaverageLast );
+
+
+  lplatch++;
+  hplatch++;
+  siglatch++;
+
   
-  sigTrig=lpTrig=hpTrig=false;
+  if (lplatch>=LPLATCHLENGTH) {lpTrig=false;}
+  if (hplatch>=HPLATCHLENGTH) {hpTrig=false;} 
+  if (siglatch>=SIGLATCHLENGTH) {sigTrig=false;}
+
   
-  if(smax>average ) 
+  if(smax>average) 
   { 
     sigTrig=true;
-    average=smax-SIGRATCHETGAP; 
+    average=smax-SIGRATCHETGAP;
+    if (siglatch > SIGLATCHLENGTH) { 
+      siglatch=0;
+    }
   }
     
   if(lpmax > lpaverage) 
   { 
     lpTrig=true;
     lpaverage=lpmax-BASERATCHETGAP;
+    if (lplatch > LPLATCHLENGTH)
+    {
+         lplatch=0;
+    }
    }
    
   if(hpmax>hpaverage ) 
   { 
     hpTrig=true;
     hpaverage=hpmax-TREBRATCHETGAP; 
+    if (hplatch > HPLATCHLENGTH)
+    {
+      hplatch=0;
+    }
    }
 
   averageLast=average;
@@ -124,7 +156,11 @@ void loop() {
       } else {
         digitalWrite(TREBPIN,HIGH);
       } //Flash in time to music
-      if(hpTrig==lpTrig==sigTrig==false)
+      if(
+        (hpTrig==false) &&
+        (lpTrig==false) &&
+        (sigTrig==false)
+        )
       {
         digitalWrite(REVERSEPIN,HIGH);
       }
